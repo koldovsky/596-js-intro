344ec5a6cf52bc0d33281ad85e8b8a8f9870f001
Vyacheslav Koldovskyy <koldovsky@koldovsky.com>
2021-05-17 17:11:30 +0000

    Sample code

---
+++ js/index.js
@@ -0,0 +1,32 @@
+// alert('Hello JS');
+
+// Коментар
+/* Коментар
+   на багато рядків */
+
+const message = 'Hello JS';
+// let message = 'Hello JS';
+// var message = 'Hello JS'; // Не бажано
+alert(message);
+
+const userName = prompt('Enter your name');
+alert('Hello ' + userName);
+
+const userAge = Number(prompt('Enter your age'));
+const nextAge = add(userAge, 1); //userAge + 1;
+alert('Next year you will be ' + nextAge);
+
+// Boolean: true, false
+
+if (userAge >= 18) { // > < <= !== === != == || &&
+    alert('You are adult');
+} else if (userAge < 3) {
+    alert('You are child');
+} else {
+    alert('You are underaged');
+}
+
+function add(a, b) {
+    const sum = a + b;
+    return sum;
+}
